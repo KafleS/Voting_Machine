d9189cf059271116c0a5c7ef3183da74438fe20e
Suman Kafle <skafle@unm.edu>
2025-05-03 17:30:17 -0600

    template factory

---
+++ src/main/java/Display/TemplateFactory.java
@@ -0,0 +1,39 @@
+package Display;
+
+import BML.Ballot;
+import BML.Proposition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TemplateFactory {
+    public static List<Template> fromBallot(Ballot ballot) {
+        List<Template> templates = new ArrayList<>();
+
+        for (int i = 0; i < ballot.getNumPropositions(); i++) {
+            Proposition p = ballot.getProposition(i);
+
+            String instruction = p.getMaxSelections() == 1
+                    ? "Please select only one option below:"
+                    : "Please select up to " + p.getMaxSelections() + " options below:";
+
+            // Use getOption(index) instead of getOptions()
+            String[] options = new String[p.getNumOptions()];
+            for (int j = 0; j < p.getNumOptions(); j++) {
+                options[j] = p.getOption(j);
+            }
+
+            templates.add(new Template(
+                    p.getTitle(),
+                    p.getDescription(),
+                    instruction,
+                    new ButtonData(i != 0),
+                    new ButtonData(i == ballot.getNumPropositions() - 1),
+                    new ButtonData(i != ballot.getNumPropositions() - 1),
+                    new QuestionInfo(options)
+            ));
+        }
+
+        return templates;
+    }
+}
